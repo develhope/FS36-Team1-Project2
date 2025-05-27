080b4d0295d9a91f7a8f18283cf24083f783025a
Alessandra <alessandraaltavilla96@gmail.com>
2025-05-27 14:40:48 +0200

    creazione CardData

--- src/Components/CardWrapped.jsx
+++ src/Components/CardWrapped.jsx
@@ -1,26 +1,5 @@
 import Card from './Card';
-
-const cardData = [
-  {
-    image: 'path/to/image1.jpg',
-    label: 'Amato dagli ospiti',
-    location: 'Stanza a Lodi - Corvetto',
-    dates: '22-24 ago',
-    hostType: 'Host privato',
-    price: '88 € per 2 notti',
-    rating: '4,81'
-  },
-  {
-    image: 'path/to/image2.jpg',
-    label: 'Amato dagli ospiti',
-    location: 'Stanza a Milano',
-    dates: '01-03 ago',
-    hostType: 'Host privato',
-    price: '107 € per 2 notti',
-    rating: '4,77'
-  },
-  // Aggiungi altre card qui
-];
+import { cardData } from '../data/CardData';
 
 const CardWrapped = () => {
   return (
@@ -29,7 +8,6 @@ const CardWrapped = () => {
         <Card
           key={index}
           image={card.image}
-          label={card.label}
           location={card.location}
           dates={card.dates}
           hostType={card.hostType}
