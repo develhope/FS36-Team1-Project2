2a0a5591de9312109fd181bce6487a85d7d14d1f
Alessandra <alessandraaltavilla96@gmail.com>
2025-05-29 10:58:38 +0200

    Fix case issue: Components → components

--- src/components/CardWrapped.jsx
+++ src/components/CardWrapped.jsx
@@ -1,22 +1,22 @@
-import Card from "./Card";
-import { cardData } from "../data/CardData";
+import Card from './Card';
+import { cardData } from '../data/CardData';
 
 const CardWrapped = () => {
-	return (
-		<div className="card-wrapper">
-			{cardData.map((card, index) => (
-				<Card
-					key={index}
-					image={card.image}
-					location={card.location}
-					dates={card.dates}
-					hostType={card.hostType}
-					price={card.price}
-					rating={card.rating}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="card-wrapper">
+      {cardData.map((card, index) => (
+        <Card
+          key={index}
+          image={card.image}
+          location={card.location}
+          dates={card.dates}
+          hostType={card.hostType}
+          price={card.price}
+          rating={card.rating}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CardWrapped;
